26a1a642c4af17ce0c8e006c29d760636f65a37d
MagicGamer <153592772+MagicGamerRMG@users.noreply.github.com>
2024-01-07 20:10:27 -0500

    Update data

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-// Set the date we're counting down to
-const countdownDate = new Date("March 17, 2024 00:00:00").getTime();
+const countdownDate = new Date("Febuary 17, 2024 00:00:00").getTime();
 
-// Update the countdown every 1 second
 const countdownElement = document.getElementById("countdown");
 
 const countdownInterval = setInterval(() => {
     const now = new Date().getTime();
     const distance = countdownDate - now;
 
-    // Time calculations
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result
     countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
-    // If the countdown is over, clear the interval
     if (distance < 0) {
         clearInterval(countdownInterval);
         countdownElement.innerHTML = "EXPIRED";
